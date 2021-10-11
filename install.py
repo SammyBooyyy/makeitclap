451a0e01e698fc72053c8a165ef267dd51d70a19
Fukk!!CodeRED <69029079+SammyBooyyy@users.noreply.github.com>
2021-10-11 09:17:29 -0400

    Add files via upload

---
+++ install.py
@@ -0,0 +1,9 @@
+import os
+
+os.system('pip3 install requests')
+os.system('pip3 install json')
+os.system('pip3 install os')
+os.system('pip3 install time')
+os.system('pip3 install speedtest-cli')
+os.system('pip3 install colorama')
+os.system('pip3 install shutil')
